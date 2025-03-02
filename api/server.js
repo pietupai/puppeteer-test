6027723b6ce10894c5b8b172c3451d054b4c2bb2
pietupai <pietupai@gmail.com>
2025-03-02 19:25:06 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ app.get('/api/scrape', async (req, res) => {
       args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath(
-        `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+        `https://github.com/Sparticuz/chromium/releases/download/v132.0.0/chromium-v132.0.0-pack.tar`
       ),
       headless: chromium.headless,
       ignoreHTTPSErrors: true,
