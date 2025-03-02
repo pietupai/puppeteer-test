c826d6758aa3457f76432d6e301fdd3e12e8bb1d
pietupai <pietupai@gmail.com>
2025-03-02 20:03:04 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -33,7 +33,7 @@ app.get('/api/scrape', async (req, res) => {
     await page.goto(fullUrl, { waitUntil: 'networkidle0' });
 
     // Wait for a while to allow the JavaScript to execute and elements to load
-    await page.waitForTimeout(55000); // Wait for max 55 seconds
+    await page.waitForTimeout(10000); // Wait for 10 seconds
 
     // Log the HTML content of the page
     const htmlContent = await page.content();
