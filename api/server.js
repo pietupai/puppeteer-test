fc84f25ec3464d619964f7209ed74301c6ab17bb
pietupai <pietupai@gmail.com>
2025-03-04 06:28:30 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -19,6 +19,7 @@ app.get('/api/scrape', async (req, res) => {
   try {
     const browser = await chromium.launch({
       args: ['--hide-scrollbars', '--disable-web-security'],
+      executablePath: '/opt/homebrew/bin/playwright-chromium', // Specify the correct path to the browser binary
       headless: true,
     });
 
