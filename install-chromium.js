61adb731d4b0ee2e3cb807d271f4eccc5d1b8a71
pietupai <pietupai@gmail.com>
2025-03-04 04:26:38 +0200

    Update install-chromium.js

--- install-chromium.js
+++ install-chromium.js
@@ -1,17 +1,14 @@
 const download = require('download');
 const path = require('path');
-const unzipper = require('unzipper');
 
 const chromiumUrl = 'https://github.com/Sparticuz/chromium/releases/download/v132.0.0/chromium-v132.0.0-pack.tar';
-const chromiumPath = path.join(__dirname, 'chromium');
+const chromiumPath = path.join(__dirname, 'node_modules/@sparticuz/chromium-min/bin');
 
 (async () => {
   try {
     console.log(`Downloading Chromium from ${chromiumUrl}...`);
     await download(chromiumUrl, chromiumPath, { extract: true });
-
     console.log(`Chromium downloaded and extracted to ${chromiumPath}`);
-    process.env.CHROME_PATH = path.join(chromiumPath, 'chromium');
   } catch (error) {
     console.error('Failed to download Chromium:', error);
   }
